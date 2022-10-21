764b0ced8e64bb6f63abe849b8cb66fcd327fde2
MoonysKR <dytjd297@gmail.com>
2022-10-21 17:28:41 +0900

    [FE] ADD | Create react app bom

---
+++ FE/bom/src/App.js
@@ -0,0 +1,14 @@
+import RouterConfiguration from "./configs/router";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="m-5 bg-yellow-500 text-blue-500 font-bold">
+        배워서 나주는 React에 Tailwind Css 적용하기
+      </div>
+      <RouterConfiguration />
+    </div>
+  );
+}
+
+export default App;
