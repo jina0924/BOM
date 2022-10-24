3e0840c59f5481674c24d8d5989931e25dbdb089
unknown <dytjd297@gmail.com>
2022-10-24 17:59:08 +0900

    [FE] FEATURE | Add Title.js & Mod index.html

---
+++ FE/bom/src/components/atoms/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { UilHospital , UilMedicalDrip , UilUserMd , UilUserNurse , UilChartLine , UilBed , UilTemperatureHalf , UilHeartbeat , UilPercentage , } from '@iconscout/react-unicons'
+
+
+
+function Title({iconTag, iconTagClassName, content, contentClassName}) {
+  if 
+  return <></>
+}
