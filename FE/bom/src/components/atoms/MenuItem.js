c8973d26c90e7228bcdd96debfe974e8d56214a1
jina0924 <jinajung73@naver.com>
2022-10-25 17:53:17 +0900

    [FE] UPDATE | Add icon in function

--- FE/bom/src/components/atoms/MenuItem.js
+++ FE/bom/src/components/atoms/MenuItem.js
@@ -10,6 +10,10 @@ function MenuItem({ menu, children }) {
   return (
     <div className="mx-5 my-10">
       {/* <span>아이콘 자리</span> */}
+      {menu === "home" && <UilLayerGroup />}
+      {menu === "patient" && <UilUsersAlt />}
+      {menu === "doctor" && <UilStethoscopeAlt />}
+      {menu === "nurse" && <UilSyringe />}
       <span className="text-font2 text-lg font-suit font-medium">
         {children}
       </span>
