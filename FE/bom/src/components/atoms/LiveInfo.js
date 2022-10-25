860488f6384997a1d76073bd602dac68aa7c2b70
unknown <dytjd297@gmail.com>
2022-10-25 10:06:27 +0900

    [FE] FEATURE | Mod Title.js & Add LiveInfo.js

---
+++ FE/bom/src/components/atoms/LiveInfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LiveInfo({ liveClassName, valueClassName, value }) {
+  return (
+    <div className="grid grid-cols-3 justify-center">
+      <span className={`col-span-1 ${liveClassName}`}>실시간</span>
+      <span className={`col-span-1 ${valueClassName}`}>{value}</span>
+    </div>
+  );
+}
+
+export default LiveInfo;
--- FE/bom/src/components/atoms/Title.js
+++ FE/bom/src/components/atoms/Title.js
@@ -79,3 +79,5 @@ function Title({ iconTag, iconTagClassName, content, contentClassName }) {
     </>
   );
 }
+
+export default Title;
