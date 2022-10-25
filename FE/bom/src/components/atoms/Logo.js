d23ae4da05662cc6aa94d77b345b8535a37cbdf4
jina0924 <jinajung73@naver.com>
2022-10-25 16:07:23 +0900

    [FE] UPDATE | Update sidebar with logo

--- FE/bom/src/components/atoms/Logo.js
+++ FE/bom/src/components/atoms/Logo.js
@@ -1,11 +1,14 @@
 import React from "react";
+import symbol from "assets/bom_symbol_50.png";
 
 function Logo() {
   return (
-    <>
-      <div></div>
-      <span className="font-righteous text-main text-3xl">BOM</span>
-    </>
+    <div className="m-8 flex items-center">
+      <img src={symbol} alt="BOM 심볼" className="inline mx-2 w-[42px]" />
+      <span className="font-righteous text-main text-3xl tracking-wider">
+        BOM
+      </span>
+    </div>
   );
 }
 
