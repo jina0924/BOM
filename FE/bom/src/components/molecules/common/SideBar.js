ebfc233cce2effb6e419fed3576c8e4839aa59f2
jina0924 <jina092400@gmail.com>
2022-10-25 16:07:23 +0900

    [FE] UPDATE | Update sidebar with logo

--- FE/bom/src/components/molecules/common/SideBar.js
+++ FE/bom/src/components/molecules/common/SideBar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo from "components/atoms/Logo";
 
 function SideBar() {
   return (
     <div className="col-span-1 rounded-l-[20px] bg-white shadow-side">
-      <div></div>
+      <Logo />
     </div>
   );
 }
