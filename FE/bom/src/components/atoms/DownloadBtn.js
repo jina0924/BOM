6d57eefb2a61c92b68821dc90192d59d38978b21
unknown <dytjd297@gmail.com>
2022-10-27 15:20:22 +0900

    [FE] FEATURE | Save tmp

--- FE/bom/src/components/atoms/DownloadBtn.js
+++ FE/bom/src/components/atoms/DownloadBtn.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { UilTable } from "@iconscout/react-unicons";
 
-function Btn({}) {
+function DownloadBtn({ onClickFunction }) {
   return (
-    <button className={btnClassName} onClick={onClickFunction}>
-      <UilTable className={iconTagClassName} />
-      <span className={contentClassName}>XLS</span>
+    <button
+      className="flex justify-center px-8 py-2 rounded-xl bg-white shadow-bg ml-5"
+      onClick={onClickFunction}
+    >
+      <UilTable className="mr-2 text-font1" />
+      <span className="text-font1">XLS</span>
     </button>
   );
 }
 
-export default Btn;
+export default DownloadBtn;
--- FE/bom/src/components/pages/PatientDetail.js
+++ FE/bom/src/components/pages/PatientDetail.js
@@ -4,14 +4,29 @@ import React from "react";
 import SideBar from "components/molecules/common/SideBar";
 import HeadBar from "components/molecules/common/Headbar";
 import PatientDetailInfo from "components/molecules/PatientDetail/PatientDetailInfo";
+import DownloadBtn from "components/atoms/DownloadBtn";
 
 function PatientDetail() {
   return (
     <div className="patient-detail grid grid-cols-6 bg-back rounded-[20px] shadow-bg w-[97vw] h-[93vh] m-7">
       <SideBar />
-      <div className="right-box col-span-5">
+      <div className="right-box col-span-5 h-[full]">
         <HeadBar />
-        <div className="components"></div>
+        <div className="filter-download-btn-box flex justify-end my-5 mx-10 h-[9.3vh]">
+          <select name="기간" id="기간">
+            기간
+          </select>
+          <DownloadBtn />
+        </div>
+        <div className="components grid grid-cols-5 mx-10 h-[74.4vh]">
+          <div className="components-left col-span-2">
+            <div className="first-component mr-5 h-1/2">
+              <PatientDetailInfo />
+            </div>
+            <div className="second-component h-1/2">2</div>
+          </div>
+          <div className="components-right col-span-3">3</div>
+        </div>
       </div>
     </div>
   );
