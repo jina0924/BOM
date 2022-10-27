ab9bea6681e068b0c0bf96216dc7de7404fccf65
권경민 <rudalsd@naver.com>
2022-10-27 15:48:12 +0900

    [EM] UPDATE | Update BMS.py

--- BMS/BMS.py
+++ BMS/BMS.py
@@ -2,10 +2,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(16, GPIO.OUT)
-GPIO.output(16,True)
+GPIO.setup(26, GPIO.OUT)
+GPIO.output(16,False)
+GPIO.output(26,True)
 
 import spidev
 import time
+import threading
 
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -15,11 +18,17 @@ def read_spi_adc(adcChannel):
     adcValue=0
     buff=spi.xfer2([1,(8+adcChannel)<<4,0])
     adcValue = ((buff[1]&3)<<8)+buff[2]
-    V = adcValue * 5 / 1024
-    print("V : ", V)
+    V = adcValue * 3.3 / 1024 / 0.2
+    print(adcChannel, "V : ", V)
     return adcValue
 
-while True:
-    read_spi_adc(0)
-    time.sleep(1)
-    
+def getV():
+    while True:
+        read_spi_adc(0)
+        read_spi_adc(1)
+        time.sleep(1)
+
+t = threading.Thread(target=getV)
+t.start()
+
+
