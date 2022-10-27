012dabe9fc73792dc9f43c16131b1d839969b0e4
jina0924 <jina092400@gmail.com>
2022-10-27 14:47:32 +0900

    [FE] UPDATE | Update height

--- FE/bom/src/components/molecules/common/Headbar.js
+++ FE/bom/src/components/molecules/common/Headbar.js
@@ -4,7 +4,7 @@ import { UilAngleDown } from "@iconscout/react-unicons";
 
 function HeadBar({ wardNum }) {
   return (
-    <div className="headbar w-full rounded-tr-[20px] bg-white shadow-head h-[110px] flex justify-end items-center px-10 gap-8">
+    <div className="headbar w-full rounded-tr-[20px] bg-white shadow-head h-[11vh] flex justify-end items-center px-10 gap-8">
       <span className="text-main text-xl font-suit font-medium">
         {wardNum}병동
       </span>
