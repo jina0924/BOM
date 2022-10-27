d12ac3ba53b55511b7b3c253802b63a47192a094
unknown <dytjd297@gmail.com>
2022-10-27 17:53:14 +0900

    [FE] FEATURE | Mod patient detail page, zoomin to show detail and zommout to show summary

--- FE/bom/src/components/molecules/PatientDetail/PatientDetailInfo.js
+++ FE/bom/src/components/molecules/PatientDetail/PatientDetailInfo.js
@@ -3,13 +3,13 @@ import Title from "components/atoms/Title.js";
 
 function PatientDetailInfo({}) {
   return (
-    <div className="patient-detail-info w-full bg-white p-3">
-      <div className="title-box flex justify-start text-3xl font-extrabold">
+    <div className="patient-detail-info w-full h-full bg-white p-3 rounded-lg shadow-box">
+      <div className="title-box text-3xl font-extrabold p-3 flex items-center">
         <Title
           iconTag="UilMedicalDrip"
-          iconTagClassName="text-sub1 w-[36px] h-[36px]"
+          iconTagClassName="text-sub1 inline mr-3"
           content="환자 상세 정보"
-          contentClassName="text-main"
+          contentClassName="text-main font-bold text-base"
         />
       </div>
       <div className="content-box"></div>
