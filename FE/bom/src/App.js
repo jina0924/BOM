c94971343ef0070e39605a9e5a0d136a7968a5e4
unknown <dytjd297@gmail.com>
2022-10-27 12:48:18 +0900

    [FE] FEATURE | Mod patient detail

--- FE/bom/src/App.js
+++ FE/bom/src/App.js
@@ -1,10 +1,14 @@
 import RouterConfiguration from "./configs/router";
 // import Main from "components/pages/Main";
+import PatientDetail from "components/pages/PatientDetail";
 
 function App() {
   return (
     <div className="App">
-      <div className="test">{/* <Main /> */}</div>
+      <div className="test">
+        {/* <Main /> */}
+        {/* <PatientDetail /> */}
+      </div>
       <RouterConfiguration />
     </div>
   );
