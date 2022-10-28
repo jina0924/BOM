c6358699014bfe168a1c7b65a56c4f0b6c53cfa4
jina0924 <jinajung73@naver.com>
2022-10-28 17:50:05 +0900

    [FE] UPDATE | Update patient list

--- FE/bom/src/components/molecules/common/PatientList.js
+++ FE/bom/src/components/molecules/common/PatientList.js
@@ -2,123 +2,118 @@ import React, { useState, useEffect } from "react";
 
 import Title from "components/atoms/Title";
 
-const tempPatientLiST = [
-  {
-    count: 7,
-    next: 3,
-    previous: 1,
-    results: [
-      {
-        pk: 1,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-      {
-        pk: 2,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-      {
-        pk: 3,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-      {
-        pk: 4,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-      {
-        pk: 5,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-      {
-        pk: 6,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-      {
-        pk: 7,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-      {
-        pk: 8,
-        name: "임진경",
-        age: 5,
-        gender: "M",
-        status_temperature: 36.5,
-        status_bpm: 129,
-        oxygen_saturation: 100,
-        nok_name: "임진경",
-        nok_phonenumber: "01012345678",
-        doctor_name: "임진경",
-      },
-    ],
-  },
-];
+const tempPatientLiST = {
+  count: 7,
+  next: 3,
+  previous: 1,
+  results: [
+    {
+      pk: 1,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+    {
+      pk: 2,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+    {
+      pk: 3,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+    {
+      pk: 4,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+    {
+      pk: 5,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+    {
+      pk: 6,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+    {
+      pk: 7,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+    {
+      pk: 8,
+      name: "임진경",
+      age: 5,
+      gender: "M",
+      status_temperature: 36.5,
+      status_bpm: 129,
+      oxygen_saturation: 100,
+      nok_name: "임진경",
+      nok_phonenumber: "01012345678",
+      doctor_name: "임진경",
+    },
+  ],
+};
 
 function PatientList({ patientListClassName }) {
-  const [patientListData, setPatientListData] = useState(tempPatientLiST);
+  // const [patientListData, setPatientListData] = useState({});
   const [patientList, setPatientList] = useState([]);
 
   useEffect(() => {
-    if (!!patientListData.results) {
-      setPatientList(patientListData.results);
-    }
-  }, [patientListData]);
-
-  console.log(patientList);
+    // setPatientListData(tempPatientLiST);
+    setPatientList(tempPatientLiST.results);
+  }, []);
 
   return (
     <div
@@ -143,16 +138,20 @@ function PatientList({ patientListClassName }) {
             </tr>
           </thead>
           <tbody>
-            {patientList.map((item, key) => (
-              <tr key={key}>
-                <td>{item.pk}</td>
-                <td>{item.name}</td>
-                <td>{item.status_temperature}</td>
-                <td>{item.status_bpm}</td>
-                <td>{item.oxygen_saturation}</td>
-                <td>{item.doctor_name}</td>
-              </tr>
-            ))}
+            {!!patientList && (
+              <>
+                {patientList.map((item, key) => (
+                  <tr key={key}>
+                    <td>{item.pk}</td>
+                    <td>{item.name}</td>
+                    <td>{item.status_temperature}</td>
+                    <td>{item.status_bpm}</td>
+                    <td>{item.oxygen_saturation}</td>
+                    <td>{item.doctor_name}</td>
+                  </tr>
+                ))}
+              </>
+            )}
           </tbody>
         </table>
       </div>
