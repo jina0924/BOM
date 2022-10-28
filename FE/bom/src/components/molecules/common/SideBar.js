6694e5a689f7d6f4b12905b431705f83fbb675ba
jina0924 <jinajung73@naver.com>
2022-10-28 13:11:08 +0900

    [FE] UPDATE | Update sidebar font size

--- FE/bom/src/components/molecules/common/SideBar.js
+++ FE/bom/src/components/molecules/common/SideBar.js
@@ -5,10 +5,10 @@ import MenuItem from "components/atoms/MenuItem";
 function SideBar() {
   return (
     <div className="col-span-1 rounded-l-[20px] bg-white shadow-side">
-      <Logo />
+      <Logo logoClassName="pt-7 pl-8 pb-2" />
       <ul className="menu-list">
         <MenuItem menu="home">홈</MenuItem>
-        <MenuItem menu="patient">환자 목록</MenuItem>
+        <MenuItem menu="patient">환자 정보</MenuItem>
         <MenuItem menu="doctor">의사 목록</MenuItem>
         <MenuItem menu="nurse">간호사 목록</MenuItem>
       </ul>
