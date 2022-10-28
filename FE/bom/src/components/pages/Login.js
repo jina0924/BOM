746cff460bbd9a2b55c51c26504859d17b5e1cf4
jina0924 <jina092400@gmail.com>
2022-10-28 11:16:19 +0900

    [FE] UPDATE | Update login form css

--- FE/bom/src/components/pages/Login.js
+++ FE/bom/src/components/pages/Login.js
@@ -9,35 +9,37 @@ function Login() {
   return (
     <div className="bg-back h-[100vh] w-[100vw] flex justify-center items-center">
       <div className="login-frame shadow-login w-[60vw] h-[70vh] rounded-[20px] flex">
-        <div className="bg-white rounded-l-[20px] w-1/2 h-full login-box p-7">
-          <Logo size="s" />
-          <h2 className="font-extrabold text-2xl text-main pt-5 pb-10 pl-8">
-            로그인
-          </h2>
-          <form action="POST">
-            <div className="user-box pl-8">
-              <input type="text" id="user_name" autocomplete="off" required />
-              <label for="user_name">
-                <span>병동 번호</span>
-              </label>
-            </div>
-            <div className="user-box pl-8">
-              <input
-                type="password"
-                id="password"
-                autocomplete="off"
-                required
+        <div className="bg-white rounded-l-[20px] w-1/2 h-full login-box flex flex-col items-center">
+          <div className="abc">
+            <Logo size="s" />
+            <h2 className="font-extrabold text-3xl text-main pt-5 pb-8">
+              로그인
+            </h2>
+            <form action="POST">
+              <div className="user-box">
+                <input type="text" id="user_name" autocomplete="off" required />
+                <label for="user_name">
+                  <span>병동 번호</span>
+                </label>
+              </div>
+              <div className="user-box">
+                <input
+                  type="password"
+                  id="password"
+                  autocomplete="off"
+                  required
+                />
+                <label for="password">
+                  <span>비밀번호</span>
+                </label>
+              </div>
+              <Btn
+                className="text-white bg-main shadow-loginbtn w-[15vw] text-sm h-9 rounded-full mt-5"
+                onClick=""
+                content="로그인"
               />
-              <label for="password">
-                <span>비밀번호</span>
-              </label>
-            </div>
-            <Btn
-              className="text-white bg-main shadow-loginbtn w-[15vw] text-sm h-8 rounded-full ml-8"
-              onClick=""
-              content="로그인"
-            />
-          </form>
+            </form>
+          </div>
         </div>
         <div className="tutorial bg-gradient-to-tr from-main to-blue rounded-r-[20px] w-1/2 h-full"></div>
       </div>
