96f9199009d0be9c98e35888ff928ca900a65c93
jina0924 <jinajung73@naver.com>
2022-10-28 11:29:11 +0900

    [FE] UDPATE | Delete logo padding

--- FE/bom/src/components/atoms/Logo.js
+++ FE/bom/src/components/atoms/Logo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import symbol from "assets/bom_symbol_50.png";
 
-function Logo({ size = "l" }) {
+function Logo({ size = "l", logoClassName }) {
   return (
     <>
       {size === "s" && (
-        <div className="p-8 flex items-center">
+        <div className={`flex items-center ${logoClassName}`}>
           <img src={symbol} alt="BOM 심볼" className="inline mx-2 w-[30px]" />
           <span className="font-righteous text-main text-xl tracking-wider">
             BOM
@@ -13,7 +13,7 @@ function Logo({ size = "l" }) {
         </div>
       )}
       {size === "l" && (
-        <div className="p-8 flex items-center">
+        <div className="flex items-center">
           <img src={symbol} alt="BOM 심볼" className="inline mx-2 w-[35px]" />
           <span className="font-righteous text-main text-2xl tracking-wider">
             BOM
