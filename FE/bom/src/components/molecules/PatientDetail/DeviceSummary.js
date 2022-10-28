870bbb25652670143aec8563e0e68f12c72b9613
unknown <dytjd297@gmail.com>
2022-10-28 13:59:20 +0900

    [FE] FEATURE | Change settings

--- FE/bom/src/components/molecules/PatientDetail/DeviceSummary.js
+++ FE/bom/src/components/molecules/PatientDetail/DeviceSummary.js
@@ -5,22 +5,22 @@ import { UilArrowResizeDiagonal } from "@iconscout/react-unicons";
 function DeviceSummary({ onZoom }) {
   return (
     <>
-      <div className="patient-device-summary w-full h-full bg-white p-3 rounded-lg shadow-box">
-        <div className="top-box flex justify-between">
-          <div className="title-box text-3xl font-extrabold p-3 flex items-center">
+      <div className="patient-device-summary w-full h-full bg-white rounded-lg shadow-box">
+        <div className="top-box flex justify-between h-1/6">
+          <div className="title-box font-extrabold flex items-start p-3">
             <Title
               iconTag="UilMonitorHeartRate"
               iconTagClassName="text-sub1 inline mr-3"
               content="디바이스 정보"
-              contentClassName="text-main font-bold text-base"
+              contentClassName="text-main font-[900] text-lg"
             />
           </div>
-          <div className="arrow-box p-3" onClick={onZoom}>
+          <div className="arrow-box p-3  h-1/6" onClick={onZoom}>
             <UilArrowResizeDiagonal className="text-font2 inline h-[16px] hover:cursor-pointer" />
           </div>
         </div>
+        <div className="content-box h-5/6 flex items-center justify-center"></div>
       </div>
-      <div className="content-box"></div>
     </>
   );
 }
