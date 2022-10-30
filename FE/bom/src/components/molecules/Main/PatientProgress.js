a94f1a49654c57c0352188abf15abc6b43617e92
jina jeong <jina092400@gmail.com>
2022-10-30 20:19:53 +0900

    [FE] UPDATE | Update main card title

--- FE/bom/src/components/molecules/Main/PatientProgress.js
+++ FE/bom/src/components/molecules/Main/PatientProgress.js
@@ -5,13 +5,15 @@ import Title from "components/atoms/Title";
 function PatientProgress() {
   return (
     <div className="patient-progress shadow-box bg-white rounded-[20px] h-[22vh] pr-5">
-      <div className="patient-progress-content py-4 px-6">
-        <Title
-          iconTag="UilChartLine"
-          iconTagClassName="text-sub1 inline mr-3"
-          content="입원 환자 추이"
-          contentClassName="text-main font-bold text-lg"
-        />
+      <div className="patient-progress-content h-full">
+        <div className="h-1/6">
+          <Title
+            iconTag="UilChartLine"
+            iconTagClassName="text-sub1 inline mr-3"
+            content="입원 환자 추이"
+            contentClassName="text-main font-bold text-lg"
+          />
+        </div>
       </div>
     </div>
   );
--- FE/bom/src/components/molecules/common/PatientList.js
+++ FE/bom/src/components/molecules/common/PatientList.js
@@ -119,13 +119,15 @@ function PatientList({ patientListClassName }) {
     <div
       className={`patient-list shadow-box bg-white rounded-[20px] h-[62vh] ${patientListClassName}`}
     >
-      <div className="patient-list-content py-4 px-6 overflow-x-auto">
-        <Title
-          iconTag="UilMedicalDrip"
-          iconTagClassName="text-sub1 inline mr-3"
-          content="환자 목록"
-          contentClassName="text-main font-bold text-lg"
-        />
+      <div className="patient-list-content h-full overflow-x-auto">
+        <div className="h-1/6">
+          <Title
+            iconTag="UilMedicalDrip"
+            iconTagClassName="text-sub1 inline mr-3"
+            content="환자 목록"
+            contentClassName="text-main font-bold text-lg"
+          />
+        </div>
         <table className="table-auto w-full">
           <thead>
             <tr>
