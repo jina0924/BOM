ee6041e4f69676a60f8892bbccdeb29d10e7fc88
jina jeong <jina092400@gmail.com>
2022-10-30 21:43:36 +0900

    [FE] UPDATE | Mod component title

--- FE/bom/src/components/molecules/Main/PatientProgress.js
+++ FE/bom/src/components/molecules/Main/PatientProgress.js
@@ -4,17 +4,16 @@ import Title from "components/atoms/Title";
 
 function PatientProgress() {
   return (
-    <div className="patient-progress shadow-box bg-white rounded-[20px] h-[22vh] pr-5">
-      <div className="patient-progress-content h-full">
-        <div className="h-1/6">
-          <Title
-            iconTag="UilChartLine"
-            iconTagClassName="text-sub1 inline mr-3"
-            content="입원 환자 추이"
-            contentClassName="text-main font-bold text-lg"
-          />
-        </div>
+    <div className="patient-progress shadow-box bg-white rounded-[20px] pr-5 h-full">
+      <div className="patient-progress-title h-1/6 py-4 px-6">
+        <Title
+          iconTag="UilChartLine"
+          iconTagClassName="text-sub1 inline mr-3"
+          content="입원 환자 추이"
+          contentClassName="text-main font-bold text-lg"
+        />
       </div>
+      <div className="patient-progress-graph h-5/6"></div>
     </div>
   );
 }
