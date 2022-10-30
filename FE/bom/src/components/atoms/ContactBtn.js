f1b70bfd91360b878d3149553a785e191aa2c9d1
MoonysKR <dytjd297@gmail.com>
2022-10-30 22:22:40 +0900

    [FE] FEATURE | Add some atoms and a molecule

---
+++ FE/bom/src/components/atoms/ContactBtn.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { UilPhone, UilEnvelope } from "@iconscout/react-unicons";
+
+function ContactBtn({ iconTag }) {
+  return (
+    <>
+      {iconTag === "UilPhone" && (
+        <div className="contact-box rounded-full w-full h-full aspect-square text-main bg-white shadow-bg hover:text-white hover:bg-main flex justify-center items-center">
+          <UilPhone size={20} />
+        </div>
+      )}
+      {iconTag === "UilEnvelope" && (
+        <div className="contact-box rounded-full w-full h-full aspect-square text-main bg-white shadow-bg hover:text-white hover:bg-main flex justify-center items-center">
+          <UilEnvelope size={20} />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ContactBtn;
