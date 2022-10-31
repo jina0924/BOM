492d540e50d6e691974bb516c874252670267b9e
unknown <dytjd297@gmail.com>
2022-10-31 16:54:27 +0900

    [FE] FEATURE | Mod logout dropbox

--- FE/bom/src/components/molecules/common/Headbar.js
+++ FE/bom/src/components/molecules/common/Headbar.js
@@ -4,14 +4,14 @@ import { UilAngleDown } from "@iconscout/react-unicons";
 
 function HeadBar({ wardNum }) {
   return (
-    <div className="headbar w-full rounded-tr-[20px] bg-white shadow-head h-[11vh] flex justify-end items-center px-10 gap-8">
+    <div className="navbar w-full rounded-tr-[20px] bg-white shadow-head h-[11vh] flex justify-end items-center px-10 gap-8">
       <span className="text-main text-lg font-suit font-medium">
         {wardNum} 병동
       </span>
       <div className="dropdown dropdown-end">
         <label
           tabIndex={0}
-          className="btn bg-white border-0 hover:bg-white focus:outline-none"
+          className="btn bg-white border-0 hover:bg-white focus:outline-none px-0"
         >
           <UilBell className="text-main inline" size={30} />
         </label>
@@ -23,7 +23,20 @@ function HeadBar({ wardNum }) {
           <li>b</li>
         </ul>
       </div>
-      <UilAngleDown className="text-main inline" size={30} />
+      <div className="dropdown dropdown-end">
+        <label
+          tabIndex={0}
+          className="btn bg-white border-0 hover:bg-white focus:outline-none px-0"
+        >
+          <UilAngleDown className="text-main inline" size={30} />
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-white rounded-box w-52 p-2 shadow-box"
+        >
+          <li>로그아웃</li>
+        </ul>
+      </div>
     </div>
   );
 }
