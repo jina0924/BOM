83404e95942a5247203b5793e91a6d92e469a386
cloudsea1106 <cloudsea1106@naver.com>
2022-10-31 18:02:54 +0900

    [BE] UPDATE | Update login

--- BE/wards/admin.py
+++ BE/wards/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import User, Ward, Nurse, Doctor, Patient, PatientStatus, Alert
+from .models import Ward, Nurse, Doctor, Patient, PatientStatus, Alert
 
 
-admin.site.register(User)
 admin.site.register(Ward)
 admin.site.register(Nurse)
 admin.site.register(Doctor)
