d03fc839317e4c2de6c81e029f90b2e1153b0716
unknown <dytjd297@gmail.com>
2022-10-31 16:27:07 +0900

    [FE] ADD | Add alarm dropbox layout

--- FE/bom/src/components/molecules/common/Headbar.js
+++ FE/bom/src/components/molecules/common/Headbar.js
@@ -8,7 +8,21 @@ function HeadBar({ wardNum }) {
       <span className="text-main text-lg font-suit font-medium">
         {wardNum} 병동
       </span>
-      <UilBell className="text-main inline" size={30} />
+      <div className="dropdown dropdown-end">
+        <label
+          tabIndex={0}
+          className="btn bg-white border-0 hover:bg-white focus:outline-none"
+        >
+          <UilBell className="text-main inline" size={30} />
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-white rounded-box w-52 p-2 shadow-box"
+        >
+          <li>a</li>
+          <li>b</li>
+        </ul>
+      </div>
       <UilAngleDown className="text-main inline" size={30} />
     </div>
   );
