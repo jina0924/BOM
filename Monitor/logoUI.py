fe4dd531a2031766cf2070b081f08ab53e52dedf
kym <lilykim37@gmail.com>
2022-10-31 17:55:32 +0900

    [EM] REFACTOR | Change ui and etc

--- Monitor/logoUI.py
+++ Monitor/logoUI.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'firstUI.ui'
+## Form generated from reading UI file 'logoUI.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.0
 ##
@@ -15,27 +15,27 @@ from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide2.QtWidgets import (QApplication, QLabel, QSizePolicy, QWidget)
+from PySide2.QtWidgets import (QApplication, QDialog, QLabel, QSizePolicy,
+    QWidget)
 import myres_rc
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(1280, 720)
-        Form.setStyleSheet(u"background-color: rgb(255, 255, 255);")
-        self.label = QLabel(Form)
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(1280, 720)
+        self.label = QLabel(Dialog)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(250, 250, 782, 271))
+        self.label.setGeometry(QRect(100, 200, 1020, 345))
         self.label.setStyleSheet(u"border-image: url(:/logo/img/logo.png);")
 
-        self.retranslateUi(Form)
+        self.retranslateUi(Dialog)
 
-        QMetaObject.connectSlotsByName(Form)
+        QMetaObject.connectSlotsByName(Dialog)
     # setupUi
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
         self.label.setText("")
     # retranslateUi
 
