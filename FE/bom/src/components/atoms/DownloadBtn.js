9bc644b29297e6ed7722e32c7da94bcfcff463aa
unknown <dytjd297@gmail.com>
2022-10-31 17:17:10 +0900

    [FE] FEATURE | Mod select tag & download btn

--- FE/bom/src/components/atoms/DownloadBtn.js
+++ FE/bom/src/components/atoms/DownloadBtn.js
@@ -4,7 +4,7 @@ import { UilTable } from "@iconscout/react-unicons";
 function DownloadBtn({ onClickFunction }) {
   return (
     <button
-      className="flex justify-center items-center px-4 py-2 rounded-xl bg-white text-font1 shadow-bg ml-5 hover:bg-[#4F9DA6]/20 hover:text-[#4F9DA6]"
+      className="flex justify-center items-center px-4 py-2 h-[2rem] rounded-xl bg-white text-font1 shadow-bg ml-5 hover:bg-[#4F9DA6]/20 hover:text-[#4F9DA6]"
       onClick={onClickFunction}
     >
       <UilTable className="pr-2 " size={22} />
