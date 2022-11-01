e4660f30b9cd168fad291f4f0f9d4796d9ec4c94
unknown <dytjd297@gmail.com>
2022-11-01 18:00:35 +0900

    [FE] FEATURE | Mod mobile responsive

--- FE/bom/src/components/molecules/PatientDetail/PatientDetailInfo.js
+++ FE/bom/src/components/molecules/PatientDetail/PatientDetailInfo.js
@@ -2,29 +2,37 @@ import React from "react";
 import Title from "components/atoms/Title.js";
 import PatientInfoKeyValue from "components/atoms/PatientInfoKeyValue";
 
-function PatientDetailInfo({}) {
+function PatientDetailInfo({ isPC = true }) {
   return (
-    <div className="patient-detail-info w-full h-full bg-white rounded-lg shadow-box">
-      <div className="title-box font-extrabold h-1/6 flex items-start py-4 px-6">
+    <div
+      className={`patient-detail-info w-full h-full bg-white rounded-lg shadow-box ${
+        !isPC && "pb-5"
+      }`}
+    >
+      <div
+        className={`title-box font-extrabold h-1/6 flex items-start ${
+          isPC ? "px-6 py-4" : "px-3 pt-2 pb-2"
+        }`}
+      >
         <Title
           iconTag="UilMedicalDrip"
           iconTagClassName="text-sub1 inline mr-3"
           content="환자 상세 정보"
-          contentClassName="text-main text-lg"
+          contentClassName={`text-main ${isPC ? "text-lg" : "text-sm"}`}
         />
       </div>
       <div className="content-box h-5/6 flex items-center justify-center">
         <div>
           <div className="first-box flex justify-center pb-1">
-            <PatientInfoKeyValue key1="환자정보" value="1" />
-            <PatientInfoKeyValue key1="이름" value="2" />
-            <PatientInfoKeyValue key1="나이" value="3" />
-            <PatientInfoKeyValue key1="성별" value="4" />
+            <PatientInfoKeyValue isPC={isPC} key1="환자정보" value="1" />
+            <PatientInfoKeyValue isPC={isPC} key1="이름" value="2" />
+            <PatientInfoKeyValue isPC={isPC} key1="나이" value="3" />
+            <PatientInfoKeyValue isPC={isPC} key1="성별" value="4" />
           </div>
           <div className="second-box flex justify-center pt-1">
-            <PatientInfoKeyValue key1="보호자" value="5" />
-            <PatientInfoKeyValue key1="보호자 연락처" value="6" />
-            <PatientInfoKeyValue key1="주치의" value="7" />
+            <PatientInfoKeyValue isPC={isPC} key1="보호자" value="5" />
+            <PatientInfoKeyValue isPC={isPC} key1="보호자 연락처" value="6" />
+            <PatientInfoKeyValue isPC={isPC} key1="주치의" value="7" />
           </div>
         </div>
       </div>
