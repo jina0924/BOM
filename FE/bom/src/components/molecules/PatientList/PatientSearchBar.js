1495957c58eaefcc1e10672639331d71f57e4652
jina0924 <jinajung73@naver.com>
2022-11-02 10:48:22 +0900

    [FE] UPDATE | Update patient search bar

--- FE/bom/src/components/molecules/PatientList/PatientSearchBar.js
+++ FE/bom/src/components/molecules/PatientList/PatientSearchBar.js
@@ -5,19 +5,19 @@ import Btn from "components/atoms/Btn";
 
 function PatientSearchBar() {
   return (
-    <div className="searchbar-box grid grid-cols-10">
-      <div className="input-box col-span-9">
+    <div className="searchbar-box flex justify-between items-center h-full py-4">
+      <div className="input-box h-full">
         <Input
           type="text"
-          className="bg-white rounded-[20px] w-full shadow-box h-full"
-          placeholder=""
+          className="bg-white rounded-[20px] w-[67vw] shadow-box px-4 h-full"
+          placeholder="환자 번호나 이름으로 검색하기"
           value=""
           onKeyUp=""
         />
       </div>
-      <div className="btn-box col-span-1">
+      <div className="btn-box col-span-1 h-full">
         <Btn
-          className="text-white bg-main shadow-loginbtn rounded-[20px]"
+          className="text-white font-semibold bg-main shadow-loginbtn rounded-[20px] w-[7.5vw] h-full"
           onClick=""
           content="검색"
         />
--- FE/bom/src/components/pages/Patients.js
+++ FE/bom/src/components/pages/Patients.js
@@ -7,15 +7,15 @@ import PatientList from "components/molecules/common/PatientList";
 
 function Patients() {
   return (
-    <div className="grid grid-cols-6 bg-back rounded-[20px] shadow-bg w-[97vw] h-[95vh] m-6 font-suit">
+    <div className="grid grid-cols-6 bg-back rounded-[20px] shadow-bg w-[97vw] h-[95vh] m-[2.5vh] font-suit">
       <SideBar />
       <div className="info-zone col-span-5">
         <HeadBar />
         <div className="flex flex-col justify-center items-center h-[84vh]">
-          <div className="h-[8vh] flex items-center">
+          <div className="h-[10vh] w-full px-8">
             <PatientSearchBar />
           </div>
-          <div className="py-10 px-8 h-[74vh]">
+          <div className="px-8 h-[74vh] pb-4">
             <PatientList />
           </div>
         </div>
