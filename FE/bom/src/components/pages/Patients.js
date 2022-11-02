732c9d73136c260a998f5ef4a42afcceec314d66
jina0924 <jina092400@gmail.com>
2022-11-02 17:26:59 +0900

    [FE] UDPATE | Mod margin for no scrollbar

--- FE/bom/src/components/pages/Patients.js
+++ FE/bom/src/components/pages/Patients.js
@@ -7,7 +7,7 @@ import PatientList from "components/molecules/common/PatientList";
 
 function Patients() {
   return (
-    <div className="grid grid-cols-6 bg-back rounded-[20px] shadow-bg w-[97vw] h-[95vh] m-[2.5vh] font-suit">
+    <div className="grid grid-cols-6 bg-back rounded-[20px] shadow-bg w-[97vw] h-[95vh] my-[2.5vh] mx-[1.5vw] font-suit">
       <SideBar />
       <div className="info-zone col-span-5">
         <HeadBar />
