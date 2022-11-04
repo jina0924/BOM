48abde80392f3873b994bb2b3ae61669d77a01bb
jina0924 <jina092400@gmail.com>
2022-11-04 16:48:42 +0900

    [FE] ADD | Add logout api

--- FE/bom/src/App.js
+++ FE/bom/src/App.js
@@ -4,27 +4,7 @@ import RouterConfiguration from "./configs/router";
 import PatientDetail from "components/pages/PatientDetail";
 import ContactBtn from "components/atoms/ContactBtn";
 
-import ls from "helper/LocalStorage";
-
-import { requestAccessToken } from "api/login";
-
 function App() {
-  // function getAccessTokenSuccess(res) {
-  //   console.log(res);
-  // }
-
-  // function getAccessTokenFail(err) {
-  //   console.log(err);
-  // }
-
-  // setInterval(() => {
-  //   // 토큰값 있으면 유효기간마다 토큰 재발급 신청
-  //   const refreshToken = ls.get("refreshToken");
-  //   if (!!refreshToken) {
-  //     requestAccessToken(getAccessTokenSuccess, getAccessTokenFail);
-  //   }
-  // }, 270000);
-
   return (
     <div className="App">
       <div className="test">
