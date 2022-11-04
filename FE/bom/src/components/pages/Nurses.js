cbc3d8fb5ed742569ee2093c23bdc6f335342a15
MoonysKR <dytjd297@gmail.com>
2022-11-05 08:00:16 +0900

    [FE] FEATURE | Add get nurses API and prop it to ProfileCard molecules, but list is empty for now

--- FE/bom/src/components/pages/Nurses.js
+++ FE/bom/src/components/pages/Nurses.js
@@ -1,11 +1,43 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 
 import SideBar from "components/molecules/common/SideBar";
 import HeadBar from "components/molecules/common/Headbar";
 import ProfileCard from "components/molecules/common/ProfileCard";
 import CustomPagination from "components/atoms/CustomPagination";
 
+import { requestNurseList } from "api/nurses";
+
 function Nurses() {
+  const [isPC, setIsPC] = useState(true);
+  const [count, setCount] = useState(0);
+  const [next, setNext] = useState(null);
+  const [previous, setPrevious] = useState(null);
+  const [nurses, setNurses] = useState([]);
+  const [now, setNow] = useState(1);
+
+  useEffect(() => {
+    window.innerWidth > 1180 ? setIsPC(true) : setIsPC(false);
+  }, []);
+
+  setInterval(() => {
+    window.innerWidth > 1180 ? setIsPC(true) : setIsPC(false);
+  }, 1000);
+
+  useEffect(() => {
+    requestNurseList("", requestPatientListSuccess, (err) => console.log(err));
+  }, []);
+
+  useEffect(() => {}, []);
+
+  const requestPatientListSuccess = (res) => {
+    console.log(res);
+    setCount(res.data.count);
+    setNext(res.data.next);
+    setPrevious(res.data.previous);
+    setNurses(res.data.results);
+    setNow(res.data.now);
+  };
+
   return (
     <div className="grid grid-cols-6 bg-back rounded-[20px] shadow-bg w-[97vw] h-[95vh] my-[2.5vh] mx-[1.5vw] font-suit">
       <SideBar />
@@ -16,6 +48,13 @@ function Nurses() {
             <span>1병동 간호사 목록</span>
           </div>
           <div className="profiles-box h-[68vh] grid grid-cols-5">
+            {nurses.map((nurse, id, array) => {
+              return (
+                <div className="profile-box col-span-1 px-2 pb-2 h-[34vh]">
+                  <ProfileCard nurse={nurse} />
+                </div>
+              );
+            })}
             <div className="profile-box col-span-1 px-2 pb-2 h-[34vh]">
               <ProfileCard />
             </div>
