21e34a9a073bacc45a511197433189f6c083dbb0
min <rudalsd@naver.com>
2022-11-08 11:23:04 +0900

    [FE] UPDATE | Update WardNum

--- FE/bom/src/components/pages/Doctors.js
+++ FE/bom/src/components/pages/Doctors.js
@@ -7,12 +7,16 @@ import CustomPagination from "components/atoms/CustomPagination";
 
 import { requestDoctorList } from "api/doctors";
 
+import ls from "helper/LocalStorage";
+
 function Doctors() {
   const [isPC, setIsPC] = useState(true);
   const [count, setCount] = useState(0);
 
   const [doctors, setDoctors] = useState([]);
   const [now, setNow] = useState(1);
+
+  const wardNum = ls.get("number");
 
   useEffect(() => {
     window.innerWidth > 1180 ? setIsPC(true) : setIsPC(false);
@@ -23,21 +27,19 @@ function Doctors() {
   }, 1000);
 
   useEffect(() => {
-    requestDoctorList("", requestDoctorListSuccess, (err) => console.log(err));
+    requestDoctorList("", requestDoctorListSuccess, err => console.log(err));
   }, []);
 
-  const requestDoctorListSuccess = (res) => {
+  const requestDoctorListSuccess = res => {
     setCount(res.data.count);
     setDoctors(res.data.results);
     setNow(res.data.now);
   };
 
-  const handlePageChange = (page) => {
+  const handlePageChange = page => {
     setNow(page);
     const params = { page: page };
-    requestDoctorList(params, requestDoctorListSuccess, (err) =>
-      console.log(err)
-    );
+    requestDoctorList(params, requestDoctorListSuccess, err => console.log(err));
   };
   return (
     <div className="grid grid-cols-6 bg-back rounded-[20px] shadow-bg w-[97vw] h-[95vh] my-[2.5vh] mx-[1.5vw] font-suit">
@@ -46,28 +48,19 @@ function Doctors() {
         <HeadBar />
         <div className="nurses-box h-[84vh] px-10">
           <div className="first-head-box h-[8vh] flex items-center text-main text-lg font-extrabold px-2">
-            <span>1병동 주치의 목록</span>
+            <span>{wardNum} 병동 주치의 목록</span>
           </div>
           <div className="profiles-box h-[68vh] grid grid-cols-5">
             {doctors.map((doctor, id, array) => {
               return (
-                <div
-                  key={id}
-                  className="profile-box col-span-1 px-2 pb-2 h-[34vh]"
-                >
+                <div key={id} className="profile-box col-span-1 px-2 pb-2 h-[34vh]">
                   <ProfileCard person={doctor} />
                 </div>
               );
             })}
           </div>
           <div className="pagination-box h-[8vh] flex items-center justify-center">
-            <CustomPagination
-              page={now}
-              itemsCount={10}
-              totalCount={count}
-              pageRange={5}
-              onChange={handlePageChange}
-            />
+            <CustomPagination page={now} itemsCount={10} totalCount={count} pageRange={5} onChange={handlePageChange} />
           </div>
         </div>
       </div>
