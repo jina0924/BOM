6a22994bc89c0fca39de52b73acb4a8f1a567c82
unknown <dytjd297@gmail.com>
2022-11-14 10:38:01 +0900

    [FE] FEATURE | Mod excel download btn

--- FE/bom/src/components/atoms/DownloadBtn.js
+++ FE/bom/src/components/atoms/DownloadBtn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { UilTable } from "@iconscout/react-unicons";
+import { UilArrowToBottom } from "@iconscout/react-unicons";
 
 function DownloadBtn({ onClickFunction }) {
   return (
@@ -7,8 +7,8 @@ function DownloadBtn({ onClickFunction }) {
       className="flex justify-center items-center px-4 py-2 h-[2rem] rounded-xl bg-white text-font1 shadow-bg ml-5 hover:bg-[#4F9DA6]/20 hover:text-[#4F9DA6]"
       onClick={onClickFunction}
     >
-      <UilTable className="pr-2 " size={22} />
-      <span className="text-xs">XLS</span>
+      <UilArrowToBottom className="pr-2 " size={22} />
+      <span className="text-xs">엑셀</span>
     </button>
   );
 }
