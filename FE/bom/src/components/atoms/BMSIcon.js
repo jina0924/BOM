30e5f30ed9f612e167adad59b383e7acc412f73b
jina jeong <jina092400@gmail.com>
2022-11-19 20:39:31 +0900

    [FE] ADD | Add patient isWarning column && Update text color

--- FE/bom/src/components/atoms/BMSIcon.js
+++ FE/bom/src/components/atoms/BMSIcon.js
@@ -17,17 +17,17 @@ function BMSIcon({ iconTag }) {
       )}
       {iconTag === "UilBatteryBolt" && (
         <div className="aspect-square h-full flex justify-center items-center bg-[#FFB400]/20 rounded-full">
-          <UilBatteryBolt className="text-[#FFB400] w-1/2 h-1/2" />
+          <UilBatteryBolt className="text-sub2 w-1/2 h-1/2" />
         </div>
       )}
       {iconTag === "UilBatteryEmpty" && (
         <div className="aspect-square h-full flex justify-center items-center bg-[#4F9DA6]/20 rounded-full">
-          <UilBatteryEmpty className="text-[#4F9DA6] w-1/2 h-1/2" />
+          <UilBatteryEmpty className="text-green w-1/2 h-1/2" />
         </div>
       )}
       {iconTag === "UilCircuit" && (
         <div className="aspect-square h-full flex justify-center items-center bg-[#1A3263]/20 rounded-full">
-          <UilCircuit className="text-[#1A3263] w-1/2 h-1/2" />
+          <UilCircuit className="text-main w-1/2 h-1/2" />
         </div>
       )}
     </>
