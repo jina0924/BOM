1fc7e00499a55b3ddc5a91f99adc3bb03a34bab2
cloudsea1106 <cloudsea1106@naver.com>
2022-11-20 20:41:38 +0900

    [DOCS] UPDATE | Update exec

---
+++ exec/DB/thundervolt_patient_status.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: thundervolt.co.kr    Database: thundervolt
+-- ------------------------------------------------------
+-- Server version	8.0.31-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `patient_status`
+--
+
+DROP TABLE IF EXISTS `patient_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient_status` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `temperature` double NOT NULL,
+  `bpm` int NOT NULL,
+  `oxygen_saturation` int NOT NULL,
+  `slope` int DEFAULT NULL,
+  `now` datetime(6) NOT NULL,
+  `patient_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `patient_status_patient_id_8df51ab3_fk_patient_id` (`patient_id`),
+  CONSTRAINT `patient_status_patient_id_8df51ab3_fk_patient_id` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2964 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient_status`
+--
+
+LOCK TABLES `patient_status` WRITE;
+/*!40000 ALTER TABLE `patient_status` DISABLE KEYS */;
+INSERT INTO `patient_status` VALUES (1,38.6,80,98,0,'2022-11-21 09:00:00.000000',28),(2,38.9,84,97,0,'2022-11-21 10:00:00.000000',28),(3,38.7,78,99,0,'2022-11-21 11:00:00.000000',28),(4,38.4,83,99,0,'2022-11-21 12:00:00.000000',28),(5,38.2,78,97,0,'2022-11-21 13:00:00.000000',28),(6,38.6,81,97,0,'2022-11-21 14:00:00.000000',28),(7,38.2,81,99,0,'2022-11-21 15:00:00.000000',28),(8,38.6,80,98,0,'2022-11-21 16:00:00.000000',28),(9,38.3,84,97,0,'2022-11-21 17:00:00.000000',28),(10,38.8,82,97,0,'2022-11-21 18:00:00.000000',28),(11,38.8,79,99,0,'2022-11-21 19:00:00.000000',28),(12,38.5,80,98,0,'2022-11-21 20:00:00.000000',28),(13,38.5,83,98,0,'2022-11-21 21:00:00.000000',28),(14,38.3,79,97,0,'2022-11-21 22:00:00.000000',28),(15,39,80,98,0,'2022-11-21 23:00:00.000000',28);
+/*!40000 ALTER TABLE `patient_status` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-20 19:59:09
